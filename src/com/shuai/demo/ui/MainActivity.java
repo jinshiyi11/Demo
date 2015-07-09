0d84020fe38426deb90854153018a61b1a8aff4f
jinshiyi11 <49662990@qq.com>
2015-07-09 20:25:21 +0800

    1.添加友盟升级

--- src/com/shuai/demo/ui/MainActivity.java
+++ src/com/shuai/demo/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import com.shuai.demo.R;
 import com.umeng.message.PushAgent;
+import com.umeng.update.UmengUpdateAgent;
 
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -21,6 +22,11 @@ public class MainActivity extends Activity {
         mPushAgent = PushAgent.getInstance(this);
         mPushAgent.onAppStart();
         mPushAgent.enable();
+        
+        //自动更新
+        UmengUpdateAgent.update(this);
+        //UmengUpdateAgent.silentUpdate(this);静默下载更新
+        UmengUpdateAgent.setUpdateOnlyWifi(false);
     }
 
 }
---
+++ src/com/shuai/demo/ui/base/BaseFragmentActivity.java
@@ -0,0 +1,22 @@
+package com.shuai.demo.ui.base;
+
+import android.app.Activity;
+
+import com.umeng.analytics.MobclickAgent;
+
+/**
+ * 增加友盟统计，所有需要使用Activity的地方都应该使用此类做基类
+ */
+public abstract class BaseFragmentActivity extends Activity {
+	@Override
+	protected void onResume() {
+		super.onResume();
+		MobclickAgent.onResume(this);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		MobclickAgent.onPause(this);
+	}
+}
