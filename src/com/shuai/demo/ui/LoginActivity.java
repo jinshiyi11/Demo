bc7601e5f7550d84310b65e4b35dbb5b7d52bbfd
jinshiyi11 <49662990@qq.com>
2015-07-13 15:56:26 +0800

    1.增加登录和注册页面

---
+++ src/com/shuai/demo/ui/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.shuai.demo.ui;
+
+import android.os.Bundle;
+
+import com.shuai.demo.R;
+import com.shuai.demo.ui.base.BaseActivity;
+
+/**
+ * 登陆界面
+ */
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_login);
+    }
+    
+}
---
+++ src/com/shuai/demo/ui/PhoneRegisterActivity.java
@@ -0,0 +1,10 @@
+package com.shuai.demo.ui;
+
+import com.shuai.demo.ui.base.BaseActivity;
+
+/**
+ * 手机注册界面
+ */
+public class PhoneRegisterActivity extends BaseActivity {
+
+}
